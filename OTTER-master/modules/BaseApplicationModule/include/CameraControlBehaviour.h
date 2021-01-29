354e5bd8114b97fb1fd7023a915be78569d72c4b
Tyler <tyler.wong@ontariotechu.net>
2021-01-28 23:28:43 -0500

    Tweaked camera controls to original startup

--- OTTER-master/modules/BaseApplicationModule/include/CameraControlBehaviour.h
+++ OTTER-master/modules/BaseApplicationModule/include/CameraControlBehaviour.h
@@ -6,13 +6,12 @@
 class CameraControlBehaviour : public IBehaviour
 {
 public:
-	void OnLoad(entt::handle entity) override;
-	void Update(entt::handle entity) override;
+    void OnLoad(entt::handle entity) override;
+    void Update(entt::handle entity) override;
 
 protected:
-	float _moveSpeed = 1.5f;
-	double _prevMouseX, _prevMouseY;
-	float _rotationX, _rotationY;
-	bool _isPressed;
-	glm::quat _initial;
+    double _prevMouseX, _prevMouseY;
+    float _rotationX, _rotationY;
+    bool _isPressed;
+    glm::quat _initial;
 };
--- OTTER-master/modules/BaseApplicationModule/src/CameraControlBehaviour.cpp
+++ OTTER-master/modules/BaseApplicationModule/src/CameraControlBehaviour.cpp
@@ -6,66 +6,59 @@
 
 
 void CameraControlBehaviour::OnLoad(entt::handle entity) {
-	_initial = entity.get<Transform>().GetLocalRotationQuat();
+    _initial = entity.get<Transform>().GetLocalRotationQuat();
 }
 
 void CameraControlBehaviour::Update(entt::handle entity)
 {
-	float dt = Timing::Instance().DeltaTime;
-	GLFWwindow* window = Application::Instance().Window;
-	double mx, my;
-	glfwGetCursorPos(window, &mx, &my);
-	Transform& transform = entity.get<Transform>();
+    float dt = Timing::Instance().DeltaTime;
+    GLFWwindow* window = Application::Instance().Window;
+    double mx, my;
+    glfwGetCursorPos(window, &mx, &my);
+    Transform& transform = entity.get<Transform>();
 
-	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS) {
-		if (!_isPressed) {
-			glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-			_isPressed = true;
-			_prevMouseX = mx;
-			_prevMouseY = my;
-		}
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS) {
+        if (!_isPressed) {
+            _isPressed = true;
+            _prevMouseX = mx;
+            _prevMouseY = my;
+        }
 
-		_rotationX += static_cast<float>(mx - _prevMouseX) * 0.5f;
-		_rotationY += static_cast<float>(my - _prevMouseY) * 0.3f;
-		glm::quat rotX = glm::angleAxis(glm::radians(-_rotationX), glm::vec3(0, 0, 1));
-		glm::quat rotY = glm::angleAxis(glm::radians(-_rotationY), glm::vec3(1, 0, 0));
-		transform.SetLocalRotation(rotX * rotY);
-		
-		_prevMouseX = mx;
-		_prevMouseY = my;
-	} else {
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-		_isPressed = false;
-	}
+        _rotationX += static_cast<float>(mx - _prevMouseX) * 0.5f;
+        _rotationY += static_cast<float>(my - _prevMouseY) * 0.3f;
+        glm::quat rotX = glm::angleAxis(glm::radians(_rotationX), glm::vec3(0, 0, 1));
+        glm::quat rotY = glm::angleAxis(glm::radians(_rotationY), glm::vec3(1, 0, 0));
+        transform.SetLocalRotation(rotX * rotY);
 
-	glm::vec3 movement = glm::vec3(0.0f);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-		movement.x += -_moveSpeed * dt;
-	}
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-		movement.x += _moveSpeed * dt;
-	}
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-		movement.z += -_moveSpeed * dt;
-	}
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-		movement.z += _moveSpeed * dt;
-	}
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-		movement.y += _moveSpeed * dt;
-	}
-	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
-		movement.y += -_moveSpeed * dt;
-	}
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
-		movement.y += -_moveSpeed * dt;
-	}
-	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
-		movement.y += _moveSpeed * dt;
-	}
+        _prevMouseX = mx;
+        _prevMouseY = my;
+    }
+    else {
+        _isPressed = false;
+    }
 
-	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)) {
-		movement *= 2.f;
-	}
-	transform.MoveLocal(movement);
+    glm::vec3 movement = glm::vec3(0.0f);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        movement.x += -1.0f * dt;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        movement.x += 1.0f * dt;
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        movement.z += -1.0f * dt;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        movement.z += 1.0f * dt;
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        movement.y += 1.0f * dt;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
+        movement.y += -1.0f * dt;
+    }
+    movement *= 2.0f;
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)) {
+        movement *= 1.5f;
+    }
+    transform.MoveLocal(movement);
 }
